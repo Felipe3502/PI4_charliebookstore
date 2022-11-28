a619415cfa1afd5909ebbfaa1ca6f5ee461f3635
Felipe Soares <felipe.soares007@hotmail.com>
2022-11-28 00:34:41 -0300

    PI4 Charlie Bookstore

---
+++ app/src/main/java/com/example/charliebookstore/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.charliebookstore.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.charliebookstore.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introdcution) {
+}
---
+++ app/src/main/java/com/example/charliebookstore/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.charliebookstore.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.charliebookstore.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
