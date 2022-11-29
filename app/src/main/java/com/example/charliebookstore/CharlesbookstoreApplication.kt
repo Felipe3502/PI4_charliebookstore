5c64104ebcdd70a2a70d97141be65802963777c2
Felipe Soares <felipe.soares007@hotmail.com>
2022-11-28 23:03:11 -0300

    PI4 Charlie Bookstore - Criando conta com E-mail

---
+++ app/src/main/java/com/example/charliebookstore/CharlesbookstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.charliebookstore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CharlesbookstoreApplication: Application() {
+
+
+
+}
