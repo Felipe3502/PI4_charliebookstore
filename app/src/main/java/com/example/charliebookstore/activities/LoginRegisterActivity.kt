8c9364091bcbd9b5482f2bc5079d6278c0e3aa57
Felipe Soares <felipe.soares007@hotmail.com>
2022-11-30 01:19:59 -0300

    PI4 Charlie Bookstore - Criando conta com E-mail - Resolvido Erro Hilt

--- app/src/main/java/com/example/charliebookstore/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/charliebookstore/activities/LoginRegisterActivity.kt
@@ -3,7 +3,9 @@ package com.example.charliebookstore.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.charliebookstore.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
